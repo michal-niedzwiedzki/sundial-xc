33655f1d522febe83314ad9ec9a5f3e5510d799b
stronger <michal.rudnicki@epsi.pl>
2012-07-25 23:32:18 +0200

    Migration script uses db silo

--- lib/db/DB.php
+++ lib/db/DB.php
@@ -57,7 +57,25 @@ final class DB {
 	 * @param string $silo can be "production" or "testing"
 	 */
 	public static function useSilo($silo) {
+		if (self::$silo and self::$silo !== $silo) {
+			throw new Exception("Silo already set to {self::$silo}");
+		}
+		if ($silo !== self::SILO_PRODUCTION and $silo !== self::SILO_TESTING) {
+			throw new Exception("Silo can only be 'production' or 'testing'");
+		}
 		self::$silo = $silo;
+	}
+
+	/**
+	 * Return database silo in use
+	 *
+	 * @return string
+	 */
+	public static function getSilo() {
+		if (!self::$silo) {
+			self::$silo = (class_exists("PHPUnit_Framework_TestCase") ? "testing" : "production");
+		}
+		return self::$silo;
 	}
 
 	/**
@@ -72,9 +90,7 @@ final class DB {
 		}
 
 		// check silo
-		$silo = self::$silo
-			? self::$silo
-			: (class_exists("PHPUnit_Framework_TestCase") ? "testing" : "production");
+		$silo = self::getSilo();
 		$config = Config::getInstance();
 		Assert::isObject($config->database);
 		Assert::isObject($config->database->$silo);
@@ -115,104 +131,6 @@ final class DB {
 		$silo = self::$silo ? self::$silo : DB::SILO_PRODUCTION;
 		foreach (DB::getMigrations() as $version) {
 			DB::migrate(TRUE, $silo, $version);
-		}
-	}
-
-	public static function initializeSystemAccount() {
-		$systemMember = array(
-			"member_id" => "ADMIN",
-			"password" => sha1("password"),
-			"member_role" => 9,
-			"security_q" => NULL,
-			"security_a" => NULL,
-			"status" => "A",
-			"member_note" => NULL,
-			"admin_note" => "Special account created during install. Ok to inactivate once an Admin Level 2 acct has been created.",
-			"join_date" => date("Y-m-d h:i:s"),
-			"expire_date" => NULL,
-			"away_date" => NULL,
-			"account_type" => "S",
-			"email_updates" => 7,
-			"balance" => 0.00
-		);
-		PDOHelper::insert(DB::MEMBERS, $systemMember);
-		$systemPerson = array(
-			"person_id" => 1,
-			"member_id" => "admin",
-			"primary_member" => "Y",
-			"directory_list" => "Y",
-			"first_name" => "Special Admin",
-			"last_name" => "Account",
-			"mid_name" => NULL,
-			"dob" => NULL,
-			"mother_mn" => NULL,
-			"email" => NULL,
-			"address_city" => DEFAULT_CITY,
-			"address_state_code" => DEFAULT_STATE,
-			"address_post_code" => DEFAULT_ZIP_CODE,
-			"address_country" => DEFAULT_COUNTRY
-		);
-		PDOHelper::insert(DB::PERSONS, $systemPerson);
-	}
-
-	public static function initializeCategories() {
-		$categories = array(
-			"Ayuda desplazamientos/transporte",
-			"Atención de mayores",
-			"Atención personas necesitadas",
-			"Cocina",
-			"Costura",
-			"Trabajos domésticos",
-			"Cuidado de animales",
-			"Jardinería",
-			"Decoración",
-			"Informática para uso domésticos",
-			"Reparaciones",
-			"Educación",
-			"Bienestar",
-			"Compañia",
-			"Familia",
-			"Atención de niños",
-			"Tramites y gestiones",
-			"Negocios",
-			"Belleza",
-			"Recreo, Ocio, Deporte",
-		);
-		foreach ($categories as $category) {
-			PDOHelper::insert(DB::CATEGORIES, array("parent_id" => NULL, "description" => $category));
-		}
-	}
-
-	public static function initializeSettings() {
-		$columns = array("id", "name", "display_name", "typ", "current_value", "options", "default_value", "max_length", "descrip", "section");
-		$values = array(
-			array('8', 'LEECH_EMAIL_URUNLOCKED', '\'Account Restriction Lifted\' Email', 'longtext', '', '', 'Restrictions on your account have been lifted.', '', 'Define email that is sent out when restrictions are lifted on an account.', '3'),
-			array('6', 'LEECH_EMAIL_URLOCKED', '\'Account Restricted\' Email', 'longtext', '', '', 'Dear Member\r\n\r\nWe have been reviewing members balances as we are concerned to ensure that trading goes back and forth on an equitable basis so that members are able to keep their accounts close to zero.  We recognise that situations sometimes occur that lead to things getting out of balance.  Therefore to assist you, we have restricted expenditure on your account for the time being. If have any queries about this, or if we can assist you in any particular way, please let us know, and we will review the situation in due course. The LETS Administrator ', '', 'Define email that is sent out when restrictions are imposed on an account.', '3'),
-			array('10', 'MEM_LIST_DISPLAY_BALANCE', 'Display Member Balance', 'bool', '', '', 'TRUE', '', 'Do you want to display member balances in the Members List? (Balances are always visible to Admins and Committee members regardless of what is set here.)', '7'),
-			array('11', 'TAKE_SERVICE_FEE', 'Enable Take Service Charge', 'bool', '', '', 'TRUE', '', 'Do you want the option of taking a service charge from members as and when?', '2'),
-			array('12', 'SHOW_INACTIVE_MEMBERS', 'Show Inactive Members in Members List', 'bool', '', '', 'FALSE', '', 'Do you want to display Inactive members in the Member List?', '7'),
-			array('13', 'SHOW_RATE_ON_LISTINGS', 'Show Rate on Listings', 'bool', '', '', 'TRUE', '', 'Do you want to display the Rate alongside the offers/wants in the main listings?', '7'),
-			array('14', 'SHOW_POSTCODE_ON_LISTINGS', 'Show Postcode on Listings', 'bool', '', '', 'TRUE', '', 'Do you want to display the PostCode alongside the offers/wants in the main listings?', '7'),
-			array('15', 'NUM_CHARS_POSTCODE_SHOW_ON_LISTINGS', 'Postcode Length (in chars)', 'int', '', '', '4', '', 'If you have elected to display the postcode on offers/wants listings, how much of the PostCode do you want to show? (the number you enter will be the number of characters displayed, so for eg if you just want to show the first 3 characters of the postcode then put 3.', '7'),
-			array('16', 'OVRIDE_BALANCES', 'Enable Balance Override', 'bool', '', '', 'FALSE', '', 'Do you want admins to have the option to override Balances on a per member basis? This can be useful during the initial site set-up for inputting existing balances. Link will appear in admin panel if set to TRUE.  Use with CAUTION to avoid the database going out of balance', '6'),
-			array('17', 'MEMBERS_CAN_INVOICE', 'Enable Member-to-Member Invoicing', 'bool', '', '', 'TRUE', '', 'Do you want to allow members to invoice one-another via the site? (The recipient is always given the option to confirm/reject payment of the invoice)', '2'),
-			array('18', 'ALLOW_IMAGES', 'Allow Members to Upload Images', 'bool', '', '', 'TRUE', '', 'Do you want to allow members to upload an image of themselves, to be displayed with their personal profile?', '4'),
-			array('19', 'SOC_NETWORK_FIELDS', 'Enable Social Networking Fields', 'bool', '', '', 'TRUE', '', 'Do you want to enable the Social Networking profile fields (Age, Sex, etc)?', '4'),
-			array('20', 'OOB_ACTION', 'Out Of Balance Behaviour', 'multiple', '', 'FATAL,SILENT', 'SILENT', '', ' If, whilst processing a trade, the database is found to be out of balance, what should the system do?\n\nFATAL = Aborts the trade and informs the user why.\n\nSILENT = Continues with trade, displays no notifications whatsoever (NOTE: you can still set the option below to have an email notification sent to the admin)', '6'),
-			array('21', 'OOB_EMAIL_ADMIN', 'Email Admin on Out Of Balance', 'bool', '', '', 'TRUE', '', 'Should the system send the Admin an email when the database is found to be out of balance?', '6'),
-			array('24', 'EMAIL_FROM', 'Email From Address', '', '', '', 'From: reply@my-domain.org', '', 'Email sent from this site will show as coming from this address', '1'),
-			array('25', 'USE_RATES', 'Use Rates Fields', 'bool', '', '', 'TRUE', '', 'If turned on, listings will include a \"Rate\" field', '7'),
-			array('26', 'TAKE_MONTHLY_FEE', 'Enable Monthly Fee', 'bool', '', '', 'TRUE', '', 'Do you want to enable Monthly Fees', '2'),
-			array('27', 'MONTHLY_FEE', 'Monthly Fee Amount', 'int', '', '', '1', '', 'How much should the Monthly Fee be?', '2'),
-			array('28', 'EMAIL_LISTING_UPDATES', 'Send Listing Updates via Email', 'bool', '', '', 'FALSE', '', 'Should users receive automatic updates for new and modified listings?', '1'),
-			array('29', 'DEFAULT_UPDATE_INTERVAL', 'Default Email Listings Update Interval', 'multiple', '', 'NEVER,WEEKLY,MONTHLY', 'NEVER', '', 'If automatic updates are sent, this is the default interval.', '1'),
-			array('34', 'ALLOW_INCOME_SHARES', 'Allow Income Sharing', 'bool', '', null, 'TRUE', '99999', 'Do you want to allow members to share a percentage of any income they generate with another account of their choosing? The member can specify the exact percentage they wish to donate.', '2'),
-			array('35', 'LEECH_NOTICE', 'Message Displayed to Leecher who tries to trade', 'longtext', '', '', 'Restrictions have been imposed on your account which prevent you from trading outwards, Please contact the administrator for more information.', '', 'Leecher sees this notice when trying to send money.', '3'),
-			array('36', 'SHOW_GLOBAL_FEES', 'Show monthly fees and service charges in global exchange view', 'bool', '', null, 'FALSE', '', 'Do you want to show monthly fees and service charges in the global exchange view? (Note: individual members will still be able to see this in their own personal exchange history).', '7'),
-		);
-		$settings = array_combine($columns, $values);
-		foreach ($settings as $s) {
-			PDOHelper::insert(DB::SETTINGS, $s);
 		}
 	}
 
@@ -279,7 +197,7 @@ final class DB {
 	 */
 	public static function migrate($upgrade, $silo, $version, $output = NULL) {
 		// check if version file writable
-		$currentVersionFile = ROOT_DIR . "/var/migrations/version.txt";
+		$currentVersionFile = ROOT_DIR . "/var/migrations/version.{$silo}.txt";
 		if (!is_writable($currentVersionFile)) {
 			throw new Exception("File $currentVersionFile must we writable");
 		}
